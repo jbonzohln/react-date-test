74913a4760653f11b4db4c5a98e07662948cc899
Jesse Bonzo <jbonzo@hln.com>
2024-03-07 14:54:37 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [state, setState] = useState({
+    date: ""
+  });
+  console.dir(state);
+
+  function onChange(e) {
+    console.dir(e);
+    setState({
+      date: e.target.value
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>State: {state.date}</div>
+      <label htmlFor='datetest'>Test:</label>
+      <input id='datetest' type="date" max="2024-03-07" value={state.date} onChange={onChange}></input>
     </div>
   );
 }
